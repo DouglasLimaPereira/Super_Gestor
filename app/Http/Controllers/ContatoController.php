a658772bcd8e6d59917334ca24938d4b826dee12
douglas <douglas.pereira@aluno.unifametro.edu.br>
2023-04-24 19:09:41 -0300

    ...

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato(){
-        echo 'Nossos Contatos';
+        return view('site.contato');
     }
 }
---
+++ resources/views/site/contato.blade.php
@@ -0,0 +1,1 @@
+<h4>CONTATO (VIEW)</h4>
