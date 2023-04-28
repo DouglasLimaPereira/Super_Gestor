d63c0e1079e607f1ea92352685c3b90746bcb573
Douglas Dg <douglas.pereira@aluno.unifametro.edu.br>
2023-04-28 10:42:51 -0300

    ajusste de rotas e views

--- routes/web.php
+++ routes/web.php
@@ -17,26 +17,24 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', [HomeController::class,'principal']);
-
-Route::get('/sobre-nos', [SobreNosController::class, 'sobre_nos']);
-
-Route::get('/contato', [ContatoController::class, 'contato']);
-
+//SITE
+Route::get('/', [HomeController::class,'principal'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'sobre_nos'])->name('site.sobre-nos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::get('/login', function(){
     return 'Login';    
-});
-Route::get('/clientes', function(){
-    return 'Clientes';    
-});
-Route::get('/fornecedores', function(){
-    return 'Fornecedores';    
-});
-Route::get('/produtos', function(){
-    return 'Produtos';    
+})->name('site.login');;
+
+//APP
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
 });
 
+Route::fallback( function() {
+    echo" A rota acessada não existe <a href=" .Route("site.index"). ">Clique Aqui</a> para ir a pagina inicial";
+});
 
 // Route::get(
 //     '/contato/{nome}/{categoria}/{assunto}/{mensagem}/',
