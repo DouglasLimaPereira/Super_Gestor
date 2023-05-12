125a8150abe31c5d67355ce8b8c4ad77a70b4e13
Douglas Dg <douglas.pereira@aluno.unifametro.edu.br>
2023-05-12 10:36:09 -0300

    implementações views

---
+++ database/migrations/2023_05_12_130541_create_ajuste_produtos_filiais_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('filiais', function (Blueprint $table) {
+            $table->id();
+            $table->string('filiais', 30);
+            $table->timestamps();
+        });
+
+        Schema::create('produto_filiais', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('filial_id');
+            $table->unsignedBigInteger('produto_id');
+            $table->float('preco_venda', 8, 2)->default(0.01);
+            $table->integer('estoque_minimo')->default(1);
+            $table->integer('estoque_maximo')->default(1);
+            $table->timestamps();
+
+            $table->foreign('filial_id')->references('id')->on('filiais');
+            $table->foreign('produto_id')->references('id')->on('produtos');
+        });
+
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->dropColumn('preco_venda');
+            $table->dropColumn('estoque_min');
+            $table->dropColumn('estoque_max');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ajuste_produtos_filiais');
+    }
+};
