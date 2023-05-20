2bab4a29248fbc578c2bf11ffdf8f01d8e684f7b
Douglas Dg <douglas.pereira@aluno.unifametro.edu.br>
2023-05-20 12:23:37 -0300

    ...

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request){
-    
+        return view('site.contato', compact('request'));
+    }
+
+    public function store( Request $request){
+
+        $request->validate([
+            
+        ]);
+
         return view('site.contato', compact('request'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class,'principal'])->name('site.index');
 Route::get('/sobre-nos', [SobreNosController::class, 'sobre_nos'])->name('site.sobre-nos');
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
-Route::post('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::post('/contato', [ContatoController::class, 'store'])->name('site.contato');
 Route::get('/login', function(){
     return 'Login';    
 })->name('site.login');;
